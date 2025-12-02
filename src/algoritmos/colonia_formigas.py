cb42a14a5f3de9a89dd25eab1b3f77e70a7c757b
Marcelo Filho <marcelo.filho@tarken.ag>
2025-12-01 21:17:15 -0300

    feat: adiciona nova estrutura para suportar otimizacao

--- src/algoritmos/colonia_formigas.py
+++ src/algoritmos/colonia_formigas.py
@@ -1,9 +1,23 @@
-import random
+class ACO:
+    def __init__(self, instancia, n_formigas=10, iter=20):
+        self.inst = instancia
+        self.n_formigas = n_formigas
+        self.iter = iter
+        self.feromonio = np.ones(instancia.matriz.shape)
 
-def resolver(dados):
-    print("\n Iniciando Colônia de Formigas...")
-    pedidos = list(dados["id_pedido"])
-    rota = random.sample(pedidos, len(pedidos))  # embaralha pedidos
-    print(f"Rota sugerida (aleatória): {rota}")
+    def run(self):
+        melhor_rota = None
+        melhor_custo = float("inf")
 
-    return rota
+        for _ in range(self.iter):
+            for f in range(self.n_formigas):
+                rota = self.construir_rota()
+                custo = Representacao(rota).custo(self.inst.matriz)
+
+                if custo < melhor_custo:
+                    melhor_custo = custo
+                    melhor_rota = rota
+            
+            self.atualizar_feromonio(melhor_rota, melhor_custo)
+
+        return Solucao(melhor_rota, melhor_custo)
--- src/main.py
+++ src/main.py
@@ -1,16 +1,19 @@
-import pandas as pd
-from algoritmos import colonia_formigas
+from algoritmos.colonia_formigas import ACO
+from modelos.instancia import Instancia
 
 def main():
     print("Otimizador de Rotas - Protótipo Inicial")
 
-    # Carregar dados de exemplo
-    dados = pd.read_csv("src/dados/exemplo.csv")
-    print("Dados de pedidos carregados:")
-    print(dados.head())
+    # Carregar dados ficticios
+    instancia = Instancia.from_csv("src/dados/pedidos.csv")
+    instancia.gerar_matriz_distancias_ficticia()
 
     # Rodar versão inicial da colônia de formigas
-    colonia_formigas.resolver(dados)
+    solver = ACO(instancia)
+    solucao = solver.run()
+
+    print("Rota:", solucao.rota)
+    print("Custo:", solucao.custo)
 
 if __name__ == "__main__":
     main()

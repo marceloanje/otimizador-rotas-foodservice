2e785710c3dbc0aed1187d7f62119e8151238ab8
Marcelo Filho <marcelo.filho@tarken.ag>
2026-02-07 22:37:27 -0300

    fix: corrige a documentação da classe Instancia para incluir 'Teste'

--- src/modelos/instancia.py
+++ src/modelos/instancia.py
@@ -3,7 +3,7 @@ import numpy as np
 import ast
 
 class Instancia:
-    """Representação de uma instância do problema VRPTW.
+    """Representação de uma instância do problema VRPTW. Teste
 
     Attributes
     ----------
